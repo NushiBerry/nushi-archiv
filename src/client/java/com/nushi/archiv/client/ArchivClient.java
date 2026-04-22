95bb8d1583918309ca163cbe85a6e32e9efdf4e3
NushiBerry <nicolas.s.correa1910@gmail.com>
2026-04-22 19:54:54 -0300

    chroe: remove template mixins and add client startup log

--- src/client/java/com/nushi/archiv/client/ArchivClient.java
+++ src/client/java/com/nushi/archiv/client/ArchivClient.java
@@ -1,10 +1,11 @@
 package com.nushi.archiv.client;
 
+import com.nushi.archiv.Archiv;
 import net.fabricmc.api.ClientModInitializer;
 
 public class ArchivClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-
+		Archiv.LOGGER.info("Archiv client initialized succesfully!");
 	}
 }
